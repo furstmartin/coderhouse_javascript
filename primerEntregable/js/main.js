9e84f8107ccadbd81492dc12fe8414adfc78f1e8
furstmartin <martin.furst@gmail.com>
2022-04-20 16:56:26 -0300

    jsPrimerEntregable

---
+++ primerEntregable/js/main.js
@@ -0,0 +1,24 @@
+let nombre = prompt("Cual es tu nombre? ").toUpperCase();
+
+while (nombre != "ESC") {
+    switch(nombre) {
+        case "PEDRO":
+            alert("Hola Pedro!");
+            break;
+        case "JUAN":
+            alert("Hola Juan!");
+            break;
+        default:
+            alert("No te conozco.");
+            break;
+    }
+
+    let llueve = prompt("Está lloviendo " + nombre + "?").toUpperCase();
+
+    if (llueve = SI) {
+        alert("Entonces, fijate de llevar paraguas.");
+    } else { 
+        alert("Sali sin cuidado entonces. ")
+    }
+
+}
